a617c6ec71dcf14c1ecaf4174f524c69128e83b2
Muhammad Mahad <mahad.pk111@gmail.com>
2026-09-15 17:02:32 +0500

    applied css

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function getWeather(event) {
   event.preventDefault();
-  const city_input = document.querySelector("#city").value;
+  const city_input = document.querySelector("#city-input").value;
   const error = document.querySelector("#error");
   if (city_input.trim() === "") {
     error.textContent = "Please enter a city!";
@@ -10,21 +10,21 @@ async function getWeather(event) {
   error.textContent = "";
   const answer = await axios(
     `https://api.weatherapi.com/v1/current.json?key=60e0a3d2f152486e950213038260606&q=${city_input}`,
-    );
+  );
   console.log(answer);
-  
+
   const temp = document.querySelector("#temp-value");
   const feel = document.querySelector("#feel-value");
   const humidity = document.querySelector("#humidity-value");
   const windKph = document.querySelector("#wind-kph-value");
-  const country = document.querySelector("#country-value")
-  const province = document.querySelector("#province-value")
+  const country = document.querySelector("#country-value");
+  const province = document.querySelector("#province-value");
   const city = document.querySelector("#city-value");
   const update = document.querySelector("#update-value");
 
-  temp.innerHTML = answer.data.current.temp_c;
+  temp.innerHTML = answer.data.current.temp_c + " °C";
   feel.innerHTML = answer.data.current.feelslike_c;
-  humidity.innerHTML = answer.data.current.humidity + "%";
+  humidity.innerHTML = answer.data.current.humidity;
   windKph.innerHTML = answer.data.current.wind_kph;
   country.innerHTML = answer.data.location.country;
   province.innerHTML = answer.data.location.region;

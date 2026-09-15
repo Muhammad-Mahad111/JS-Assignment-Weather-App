ca7c8740b42ab6ea1440b0115fc3c653a27376ac
Muhammad Mahad <mahad.pk111@gmail.com>
2026-09-15 08:29:13 +0500

    weather app logic completed

---
+++ script.js
@@ -0,0 +1,33 @@
+async function getWeather(event) {
+  event.preventDefault();
+  const city_input = document.querySelector("#city").value;
+  const error = document.querySelector("#error");
+  if (city_input.trim() === "") {
+    error.textContent = "Please enter a city!";
+    error.style.color = "red";
+    return;
+  }
+  error.textContent = "";
+  const answer = await axios(
+    `https://api.weatherapi.com/v1/current.json?key=60e0a3d2f152486e950213038260606&q=${city_input}`,
+    );
+  console.log(answer);
+  
+  const temp = document.querySelector("#temp-value");
+  const feel = document.querySelector("#feel-value");
+  const humidity = document.querySelector("#humidity-value");
+  const windKph = document.querySelector("#wind-kph-value");
+  const country = document.querySelector("#country-value")
+  const province = document.querySelector("#province-value")
+  const city = document.querySelector("#city-value");
+  const update = document.querySelector("#update-value");
+
+  temp.innerHTML = answer.data.current.temp_c;
+  feel.innerHTML = answer.data.current.feelslike_c;
+  humidity.innerHTML = answer.data.current.humidity + "%";
+  windKph.innerHTML = answer.data.current.wind_kph;
+  country.innerHTML = answer.data.location.country;
+  province.innerHTML = answer.data.location.region;
+  city.innerHTML = answer.data.location.name;
+  update.innerHTML = answer.data.current.last_updated;
+}

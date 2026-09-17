b62e2380c000bd433f6f27b5f2bdefc3a1930d52
Muhammad Mahad <mahad.pk111@gmail.com>
2026-09-17 22:37:52 +0500

    added error handling
    for incorrect city name.

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
 async function getWeather(event) {
   event.preventDefault();
   const city_input = document.querySelector("#city-input").value;
-  const error = document.querySelector("#error");
+  const errorP = document.querySelector("#error");
+  let answer;
+  errorP.textContent = "";
   if (city_input.trim() === "") {
-    error.textContent = "Please enter a city!";
-    error.style.color = "red";
+    errorP.textContent = "Please enter a city!";
+    errorP.style.color = "red";
     return;
+  } else {
+    try {
+      answer = await axios(
+      `https://api.weatherapi.com/v1/current.json?key=60e0a3d2f152486e950213038260606&q=${city_input}`,
+      );
+      const temp = document.querySelector("#temp-value");
+      const feel = document.querySelector("#feel-value");
+      const humidity = document.querySelector("#humidity-value");
+      const windKph = document.querySelector("#wind-kph-value");
+      const country = document.querySelector("#country-value");
+      const province = document.querySelector("#province-value");
+      const city = document.querySelector("#city-value");
+      const update = document.querySelector("#update-value");
+
+      temp.innerHTML = answer.data.current.temp_c + " °C";
+      feel.innerHTML = answer.data.current.feelslike_c;
+      humidity.innerHTML = answer.data.current.humidity;
+      windKph.innerHTML = answer.data.current.wind_kph;
+      country.innerHTML = answer.data.location.country;
+      province.innerHTML = answer.data.location.region;
+      city.innerHTML = answer.data.location.name;
+      update.innerHTML = answer.data.current.last_updated;
+    } catch (error) {
+      console.log(error.response.data.error.message);
+      errorP.innerHTML = error.response.data.error.message;
+      errorP.style.color = "red";
+    }
   }
-  error.textContent = "";
-  const answer = await axios(
-    `https://api.weatherapi.com/v1/current.json?key=60e0a3d2f152486e950213038260606&q=${city_input}`,
-  );
-  console.log(answer);
-
-  const temp = document.querySelector("#temp-value");
-  const feel = document.querySelector("#feel-value");
-  const humidity = document.querySelector("#humidity-value");
-  const windKph = document.querySelector("#wind-kph-value");
-  const country = document.querySelector("#country-value");
-  const province = document.querySelector("#province-value");
-  const city = document.querySelector("#city-value");
-  const update = document.querySelector("#update-value");
-
-  temp.innerHTML = answer.data.current.temp_c + " °C";
-  feel.innerHTML = answer.data.current.feelslike_c;
-  humidity.innerHTML = answer.data.current.humidity;
-  windKph.innerHTML = answer.data.current.wind_kph;
-  country.innerHTML = answer.data.location.country;
-  province.innerHTML = answer.data.location.region;
-  city.innerHTML = answer.data.location.name;
-  update.innerHTML = answer.data.current.last_updated;
+  // console.log(answer);
 }
